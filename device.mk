e90068d75420f7bdc0ad64d5698804f4ca97b044
itswill00 <anstykx00@gmail.com>
2026-06-21 20:27:11 +0700

    annibale: Localize excluded-input-devices.xml
    Copy excluded-input-devices.xml from the ROM dump and place it in
    device configs to prevent build errors caused by missing source files in
    the common hardware/xiaomi tree.
    
    Change-Id: I37498de140e719ef110e5425c114b9c20c828f38
    Signed-off-by: itswill00 <anstykx00@gmail.com>

--- device.mk
+++ device.mk
@@ -472,7 +472,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi
 
 PRODUCT_COPY_FILES += \
-    hardware/xiaomi/aidl/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
